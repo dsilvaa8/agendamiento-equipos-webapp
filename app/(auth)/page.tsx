1f57c8c43d4170a9bd338aa875b15f57deec2871
WDCPrado <wellintoncosta.10@hotmail.com>
2023-11-16 14:16:07 -0300

    feat: new changes

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -55,7 +55,7 @@ export default function LoginCard() {
       const data = await response.data;
 
       if (data.status === 200) {
-        return router.push("/admin");
+        return router.push("/admin/home");
       }
     } catch (e: any) {
       console.log(e.request.status);
--- app/admin/(routes)/page.tsx
+++ app/admin/(routes)/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 
 import DashboardPage from "./home/page";
 
