7f7788c64468e25d4bfbd64f52a805ebfd0e999e
WDCPrado <wellintoncosta.10@hotmail.com>
2023-11-16 12:43:38 -0300

    feat: dashboardpage

--- app/admin/(routes)/page.tsx
+++ app/admin/(routes)/page.tsx
@@ -1,26 +1,9 @@
 "use client";
-import { fetchLaboratoriesData, fetchPcsData } from "@/app/actions/fetchData";
-import { Overview } from "@/components/overview";
-import { mapLaboratoriesData, mapPcsData } from "@/lib/utils";
 
-const DashboardPage = async () => {
-  // Llama a las funciones para obtener los datos de laboratorios y PCs
-  const apiLaboratoriesData = await fetchLaboratoriesData();
-  const apiPcsData = await fetchPcsData();
+import DashboardPage from "./home/page";
 
-  // Mapea los datos de la API al formato deseado utilizando las funciones mapLaboratoriesData y mapPcsData
-  const laboratoriesData = mapLaboratoriesData(apiLaboratoriesData);
-  const pcsData = mapPcsData(apiPcsData);
-
-  return (
-    <div className="flex flex-col mx-auto w-full items-center justify-between gap-3 py-5">
-      <h2>Bienvenido</h2>
-      <div className="flex md:flex-row flex-col p-10 w-full gap-5">
-        <Overview data={pcsData} title="Notebooks" />
-        <Overview data={laboratoriesData} title="Laboratorios" />
-      </div>
-    </div>
-  );
+const Page = async () => {
+  return <DashboardPage />;
 };
 
-export default DashboardPage;
+export default Page;
